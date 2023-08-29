c3a796bd77b427486ab9004c8ce8097088e3aa0e
Ali Fatavi <1386alifatavi@gmail.com>
2023-08-30 00:09:16 +0330

    setup project

--- src/main/java/net/alifatavi/fatmod/FatModDataGenerator.java
+++ src/main/java/net/alifatavi/fatmod/FatModDataGenerator.java
@@ -3,7 +3,7 @@ package net.alifatavi.fatmod;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
-public class ExampleModDataGenerator implements DataGeneratorEntrypoint {
+public class FatModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
