8267c7673ce4c5e1c28644d32ef24d7c4bcebd68
Ali Fatavi <1386alifatavi@gmail.com>
2023-08-30 17:55:26 +0330

    setup project

---
+++ src/main/java/net/alifatavi/fatmod/FatModClient.java
@@ -0,0 +1,10 @@
+package net.alifatavi.fatmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FatModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
